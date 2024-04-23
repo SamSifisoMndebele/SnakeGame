55f475593b7e392780c1b29e1b7fcf6acaa7e68c
Sam Sifiso Mndebele <sams.mndebele@gmail.com>
2024-04-23 21:20:52 +0200

    v1.0

---
+++ src/GameFrame.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.io.Serial;
+
+public class GameFrame extends JFrame {
+	@Serial
+	private static final long serialVersionUID = 1L;
+
+	GameFrame(){
+		this.add(new GamePanel());
+		this.setTitle("Snake Game");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+	}
+	
+	public static void main(String[] args) {
+		new GameFrame();
+	}
+}
